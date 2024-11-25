fc8935d9f3d53bd81b1d048b56439406ef1d11cf
ebahena98 <edgarbahena1998@gmail.com>
2024-11-25 05:23:08 -0800

    changing stuff

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(8080, () => {
+    console.log('Server is listening on port 8080');
+});
+
+// SENDING FILE TO FRONTEND
+app.get()
